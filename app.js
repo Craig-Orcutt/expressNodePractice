2321fda363b7ee73a26c4c4fb5b1e8de9bac43b3
Craig Orcutt <craigaorcutt@gmail.com>
2018-03-13 11:32:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const logParams = (req,res, next) => {
+    console.log('this is middleware');
+    console.log('req.params', req.params.id);
+    console.log('req.url from our middleware', req.url );
+    next();
+}
+
+// middlewares
+app.use(logParams);
+
+// respond with 'hello world' when a  get request is made to the homepage
+app.get("/", function(req,res){
+    res.send('hello world');
+})
+
+app.listen(8080, ()=>{
+    console.log('listening on port 8080');
+    
+})
